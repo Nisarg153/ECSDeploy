5b75b13eee03ca842781169c8546707f08e1a3f6
Nisarg153 <nisarg150301@gmail.com>
2025-04-05 17:09:37 -0400

    ! removed

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Boot API in ECS Cluster!";
+        return "Hello from Spring Boot API in ECS Cluster";
     }
 }
